6b2246b1b860b96d721a1c7423e3971689dffca3
cc1901 <chekai1985@gmail.com>
2017-02-13 08:17:11 +0800

    update capture variable in windows batch

--- output.bat
+++ output.bat
@@ -1,4 +1,6 @@
 
 LAST_DATE=`java -jar -XX:MaxMetaspaceSize=100M build/libs/date-util-1.0.jar dd/MM/yyyy`
 
-echo ${LAST_DATE}
+for /f %%i in ('java -jar date-util-1.0.jar MM/dd/yy') do set PREVIOUS_MONTH_LAST_DATE=%%i
+
+echo %PREVIOUS_MONTH_LAST_DATE%
